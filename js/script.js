32f93a4a9a165bdb77fe9932df62a375e0d3a528
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-09-02 19:14:21 +0545

    Responsive homepage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 const swiper = new Swiper(".homepage_img", {
   loop: true,
   grabCursor: true,
+  slidesPerView: 1,
 
   pagination: {
-    el: ".swiper-pagination",
+    // el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
 
+    // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
   autoplay: {
-   delay: 5000,
+   delay: 3000,
  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+    }
+  }
 
 });
