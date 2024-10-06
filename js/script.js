e8205133cd865de2d67ac93ff9fd6f861ac020f1
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-10-06 19:53:57 +0545

    Added small devices nav

--- js/script.js
+++ js/script.js
@@ -1,37 +1,57 @@
-const swiper = new Swiper(".homepage_img", {
-  loop: true,
-  grabCursor: true,
-  slidesPerView: 1,
+// const swiper = new Swiper(".homepage_img", {
+//   loop: true,
+//   grabCursor: true,
+//   slidesPerView: 1,
 
-  pagination: {
-    // el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+//   pagination: {
+//     // el: ".swiper-pagination",
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
 
-    // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
 
-  autoplay: {
-   delay: 3000,
- },
+//   autoplay: {
+//     delay: 3000,
+//   },
 
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 1,
-    }
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 1,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 1,
+//     },
+//   },
+// });
+
+const side_menu = document.getElementById("side_menu");
+const hamburger_menu = document.getElementById("hamburger");
+const cancel_menu = document.getElementById("cancel_menu");
+const toggle = document.getElementById("toggle");
+let isActive = true;
+let click = 1;
+
+hamburger_menu.addEventListener("click", () => {
+  side_menu.style.display = "block";
+  click++;
+  if (isActive == true) {
+    toggle.addEventListener("click", () => {
+      side_menu.style.display = "none";
+    });
   }
+});
 
+cancel_menu.addEventListener("click", () => {
+  side_menu.style.display = "none";
 });
